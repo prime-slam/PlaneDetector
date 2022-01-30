304b77b1029acb2f19463618683cefbc38c1e7d1
DmiitriyJarosh <dimajarosh@gmail.com>
2022-01-30 16:03:42 +0300

    Made sse labels generator independent

--- scripts/kitti/sse_labels_generator.py
+++ scripts/kitti/sse_labels_generator.py
@@ -1,6 +1,26 @@
 import sys
 
-from src.utils.colors import get_random_color
+import numpy as np
+
+
+def color_to_string(color_arr):
+    # np.array2string is critically slow
+    return "{0},{1},{2}".format(color_arr[0], color_arr[1], color_arr[2])
+
+
+UNSEGMENTED_PCD_COLOR = [0, 0, 0]  # [0.5, 0.5, 0.5]
+produced_colors_set = {color_to_string(UNSEGMENTED_PCD_COLOR)}
+
+
+def get_random_color():
+    random_color = np.asarray(UNSEGMENTED_PCD_COLOR)
+    while color_to_string(random_color) in produced_colors_set:
+        random_color = np.asarray([int(x) for x in np.random.choice(range(256), size=3)])
+
+    produced_colors_set.add(color_to_string(random_color))
+
+    return random_color
+
 
 if __name__ == "__main__":
     amount_to_generate = int(sys.argv[1])
