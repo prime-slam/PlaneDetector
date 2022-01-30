c52b428213c8ce1bd57f6ffe4cc95ce484d54021
DmiitriyJarosh <dimajarosh@gmail.com>
2022-01-30 21:46:33 +0300

    Fix annotations mapping and add frames with center in bbox recording

--- scripts/kitti/map_builder.py
+++ scripts/kitti/map_builder.py
@@ -106,29 +106,50 @@ def get_annot_path_pairs(annot_path):
 
 def map_annotations_to_map(
         map_pcd: o3d.geometry.PointCloud,
+        loader: KittiLoader,
+        calib_matrix: np.array,
+        map_poses: np.array,
         path_to_annot: str,
+        path_to_annot_poses: str,
         labels_filename: str
 ) -> np.array:
     map_points_count = np.asarray(map_pcd.points).shape[0]
-    kd_tree = KDTree(np.asarray(map_pcd.points))
-    next_free_id = 1  # skip 0 for void label
+    map_tree = KDTree(np.asarray(map_pcd.points))
+    last_used_id = 0  # skip 0 for void label
     map_annot_indices = np.zeros(map_points_count, dtype=np.intc)
+    frames_poses = load_poses(path_to_annot_poses)
     for (data_path, annot_path) in get_annot_path_pairs(path_to_annot):
-        data_pcd = o3d.io.read_point_cloud(data_path)
+        segment_pcd = o3d.io.read_point_cloud(data_path)
         annot = SSEAnnotation(annot_path)
-        planes_indices = annot.get_all_planes_ids()
-        data_indices = kd_tree.query(np.asarray(data_pcd.points))[1]  # take only indices, skip distances
-        for plane_indices in planes_indices:
-            plane_id = next_free_id
-            next_free_id += 1
-            plane_map_indices = data_indices[plane_indices]
-            map_annot_indices[plane_map_indices] = plane_id
+        segment_labels = annot.load_labels()
+        is_zero_label = segment_labels != 0
+        segment_labels = (segment_labels + last_used_id) * is_zero_label
+        last_used_id = max(np.max(segment_labels), last_used_id)
+        start_frame = int(os.path.split(data_path)[-1].split("_")[1])
+        end_frame = int(os.path.split(data_path)[-1].split("_")[2])
+        segment_tree = KDTree(np.asarray(segment_pcd.points))
+        for frame_num in range(start_frame, min(end_frame + 1, loader.get_frame_count() - 1), 10):
+            frame_pcd = loader.read_pcd(frame_num)
+            frame_segment_transform_matrix = frames_poses[frame_num]
+            frame_pcd_mapped_to_segment = cloud_to_map(frame_pcd, frame_segment_transform_matrix, calib_matrix)
+            frame_segment_indices = segment_tree.query(np.asarray(frame_pcd_mapped_to_segment.points))[1]
+            frame_labels = segment_labels[frame_segment_indices]
+
+            frame_pcd = loader.read_pcd(frame_num)
+            frame_map_transform_matrix = map_poses[frame_num]
+            frame_pcd_mapped_to_map = cloud_to_map(frame_pcd, frame_map_transform_matrix, calib_matrix)
+            frame_map_indices = map_tree.query(np.asarray(frame_pcd_mapped_to_map.points))[1]
+            frame_labels_not_null_indices = np.where(frame_labels > 0)[0]
+            frame_map_indices_not_null = frame_map_indices[frame_labels_not_null_indices]
+            map_annot_indices[frame_map_indices_not_null] = frame_labels[frame_labels_not_null_indices]
+
+        print("Annotations from {} loaded!".format(data_path))
 
     if labels_filename.endswith(".npy"):
         np.save(labels_filename, map_annot_indices)
     else:
         SSEAnnotation.save_to_file(map_annot_indices, labels_filename)
-    print("Last used plane id: {}".format(next_free_id - 1))
+    print("Last used plane id: {}".format(last_used_id - 1))
 
     return map_annot_indices
 
@@ -145,23 +166,38 @@ def get_bboxes_bounds_of_parts(full_bbox: o3d.geometry.AxisAlignedBoundingBox, s
     return bboxes_bounds
 
 
+def get_centers_pos_in_map(loader: KittiLoader, poses, calib_matrix: np.array) -> np.array:
+    res = []
+    for frame_id in range(loader.get_frame_count()):
+        pcd = o3d.geometry.PointCloud()
+        pcd.points = o3d.utility.Vector3dVector(np.asarray([[0., 0., 0.]]))
+        transform_matrix = poses[frame_id] @ calib_matrix
+        pcd.transform(transform_matrix)
+        res.append(np.asarray(pcd.points)[0, :])
+
+    return np.asarray(res)
+
+
 if __name__ == "__main__":
     path_to_data = sys.argv[1]
     path_to_poses = sys.argv[2]
     path_to_calib = sys.argv[3]
     path_to_annot = sys.argv[4]
-    parts_output_path = sys.argv[5]
+    path_to_annot_poses = sys.argv[5]
+    parts_output_path = sys.argv[6]
     debug = False
+    calc_included_frames = True
 
     map_filename = "map.pcd"
     annot_filename = "map.pcd.labels.npy"
 
+    loader = KittiLoader(path_to_data)
+    poses = load_poses(path_to_poses)
+    calib_matrix = load_calib_matrix(path_to_calib)
+
     if os.path.isfile(map_filename):
         map_pcd = o3d.io.read_point_cloud(map_filename)
     else:
-        loader = KittiLoader(path_to_data)
-        poses = load_poses(path_to_poses)
-        calib_matrix = load_calib_matrix(path_to_calib)
         map_pcd = o3d.geometry.PointCloud()
 
         for cloud_id in range(0, loader.get_frame_count(), 10):
@@ -182,11 +218,27 @@ if __name__ == "__main__":
     if os.path.isfile(annot_filename):
         map_annot_labels = np.load(annot_filename)
     else:
-        map_annot_labels = map_annotations_to_map(map_pcd, path_to_annot, annot_filename)
+        map_annot_labels = map_annotations_to_map(
+            map_pcd,
+            loader,
+            calib_matrix,
+            poses,
+            path_to_annot,
+            path_to_annot_poses,
+            annot_filename
+        )
     print("Annotations loaded to map")
 
     if debug:
-        map_annotations_to_map(low_map, path_to_annot, "super_low_map.pcd.labels")
+        map_annotations_to_map(
+            low_map,
+            loader,
+            calib_matrix,
+            poses,
+            path_to_annot,
+            path_to_annot_poses,
+            "super_low_map.pcd.labels"
+        )
         print("Annotations loaded to low map")
 
     map_bbox = map_pcd.get_axis_aligned_bounding_box()
@@ -194,9 +246,12 @@ if __name__ == "__main__":
     print(bbox_points)
     bboxes_bounds = get_bboxes_bounds_of_parts(map_bbox, step=150)
     map_points = np.asarray(map_pcd.points)
+    centers_points = get_centers_pos_in_map(loader, poses, calib_matrix)
     for bbox_bounds in bboxes_bounds:
         min_x, min_y, min_z = bbox_bounds[0]
         max_x, max_y, max_z = bbox_bounds[1]
+        bbox_str = "{0}_{1}_{2}_{3}".format(int(min_x), int(max_x), int(min_z), int(max_z))
+
         fit_in_part = np.zeros_like(map_points, dtype=bool)
         fit_in_part[:, 0] = np.logical_and(map_points[:, 0] < max_x, map_points[:, 0] >= min_x)
         fit_in_part[:, 1] = np.logical_and(map_points[:, 1] < max_y, map_points[:, 1] >= min_y)
@@ -204,7 +259,7 @@ if __name__ == "__main__":
         part_indices = np.where(np.all(fit_in_part, axis=-1))[0]
 
         if part_indices.size < 1000:
-            print("BBox {0}_{1}_{2}_{3} is too small!".format(int(min_x), int(max_x), int(min_z), int(max_z)))
+            print("BBox {} is too small!".format(bbox_str))
             continue
 
         part_pcd = map_pcd.select_by_index(part_indices)
@@ -216,18 +271,24 @@ if __name__ == "__main__":
         # remove added zero
         part_annot = unique_labels_indices[1:]
 
-        part_pcd_filename = "part_{0}_{1}_{2}_{3}_u{4}.pcd".format(
-            int(min_x), int(max_x),
-            int(min_z), int(max_z),
-            unique_labels.size
-        )
+        bbox_str_with_unique = "{0}_u{1}".format(bbox_str, unique_labels.size)
+
+        if calc_included_frames:
+            centers_fit_in_part = np.zeros_like(centers_points, dtype=bool)
+            centers_fit_in_part[:, 0] = np.logical_and(centers_points[:, 0] < max_x, centers_points[:, 0] >= min_x)
+            centers_fit_in_part[:, 1] = np.logical_and(centers_points[:, 1] < max_y, centers_points[:, 1] >= min_y)
+            centers_fit_in_part[:, 2] = np.logical_and(centers_points[:, 2] < max_z, centers_points[:, 2] >= min_z)
+            fit_centers_indices = np.where(np.all(centers_fit_in_part, axis=-1))[0]
+            np.savetxt(
+                os.path.join(parts_output_path, "part_{}.frames.txt".format(bbox_str_with_unique)),
+                fit_centers_indices,
+                fmt='%i'
+            )
+
+        part_pcd_filename = "part_{}.pcd".format(bbox_str_with_unique)
         part_pcd_filename = os.path.join(parts_output_path, part_pcd_filename)
         o3d.io.write_point_cloud(part_pcd_filename, part_pcd)
         SSEAnnotation.save_to_file(part_annot, "{}.labels".format(part_pcd_filename))
-        print("BBox {0}_{1}_{2}_{3}_u{4} ready!".format(
-            int(min_x), int(max_x),
-            int(min_z), int(max_z),
-            unique_labels.size
-        ))
+        print("BBox {} ready!".format(bbox_str_with_unique))
 
     print("All parts prepared!")
