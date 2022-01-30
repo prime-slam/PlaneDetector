3e5aa31d36fd3fa13b1818e1200eef00b59f31ff
DmiitriyJarosh <dimajarosh@gmail.com>
2022-01-30 10:31:55 +0300

    Fix sse label generator script

--- scripts/kitti/sse_labels_generator.py
+++ scripts/kitti/sse_labels_generator.py
@@ -7,4 +7,4 @@ if __name__ == "__main__":
     for i in range(amount_to_generate):
         color = get_random_color()
         hex_color_str = "#{0:02X}{1:02X}{2:02X}".format(color[0], color[1], color[2])
-        print('{{"label": "Plane_{0}", "color": "{1}", "icon": "Road"}},'.format(i, hex_color_str))
+        print('      {{"label": "Plane_{0}", "color": "{1}", "icon": "Road"}},'.format(i, hex_color_str))
