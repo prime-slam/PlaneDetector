2686d5c27a5d09eb666afa79581f38ce51d32bf2
DmiitriyJarosh <dimajarosh@gmail.com>
2022-02-14 14:37:34 +0300

    Add CARLA frame annotator

--- scripts/carla/pointclouds_processor.py
+++ scripts/carla/pointclouds_processor.py
@@ -5,9 +5,6 @@ import numpy as np
 import open3d as o3d
 import ast
 
-
-# Check https://carla.readthedocs.io/en/latest/ref_sensors/#semantic-segmentation-camera for label explanation
-# good_labels = set([0, 1])
 from pykdtree.kdtree import KDTree
 
 
@@ -23,6 +20,15 @@ def visualize_pcd_labels(pcd: o3d.geometry.PointCloud, labels: np.array, filenam
         o3d.io.write_point_cloud(filename, pcd_for_vis)
 
 
+def pcd_from_carla_line(line: str) -> o3d.geometry.PointCloud:
+    points_packed = ast.literal_eval(line.split(",|,")[1])
+    points = np.asarray([point_label_id[0] for point_label_id in points_packed])
+    pcd = o3d.geometry.PointCloud()
+    pcd.points = o3d.utility.Vector3dVector(points)
+
+    return pcd
+
+
 def build_map(data_path) -> o3d.geometry.PointCloud:
     map_pcd = o3d.geometry.PointCloud()
     with open(data_path) as data_file:
@@ -30,20 +36,8 @@ def build_map(data_path) -> o3d.geometry.PointCloud:
             if index % 10 != 0:
                 continue
 
-            points_packed = ast.literal_eval(line.split(",|,")[1])
-            points = np.asarray([point_label_id[0] for point_label_id in points_packed])
-            # ids = [point_label_id[2] for point_label_id in points_packed]
-            # labels = [point_label_id[1] for point_label_id in points_packed]
-
-            # for i, label in enumerate(labels):
-            #     if label in good_labels:
-            #         filtered.append(points[i])
-            #         filtered_ids.append(ids[i])
-
-            pcd = o3d.geometry.PointCloud()
-            pcd.points = o3d.utility.Vector3dVector(points)
-            map_pcd += pcd
-
+            frame_pcd = pcd_from_carla_line(line)
+            map_pcd += frame_pcd
             print("Cloud {} loaded!".format(index + 1))
 
         map_pcd = map_pcd.voxel_down_sample(0.2)
@@ -57,12 +51,10 @@ def annotate_map(map_pcd: o3d.geometry.PointCloud, annot_path: str) -> np.array:
     map_points_count = np.asarray(map_pcd.points).shape[0]
     map_labels = np.zeros(map_points_count, dtype=int)
     max_used_label = -1
-    # mesh_pcds = []
     for index, mesh_name in enumerate(mesh_names):
         mesh_pcd_filename = "{}.pcd".format(mesh_name)
         mesh_labels_filename = "{}.npy".format(mesh_name)
         mesh_pcd = o3d.io.read_point_cloud(os.path.join(annot_path, mesh_pcd_filename))
-        # mesh_pcds.append(mesh_pcd)
         mesh_labels = np.load(os.path.join(annot_path, mesh_labels_filename))
         mesh_labels += max_used_label + 1
         max_used_label = max(max_used_label, np.max(mesh_labels))
@@ -81,12 +73,24 @@ def annotate_map(map_pcd: o3d.geometry.PointCloud, annot_path: str) -> np.array:
 
         print("{0} is ready! ({1}/{2})".format(mesh_name, index + 1, len(mesh_names)))
 
-    # colors = np.concatenate([np.asarray([[0, 0, 0]]), np.random.rand(len(mesh_pcds), 3)])
-    # for index, mesh_pcd in enumerate(mesh_pcds):
-    #     mesh_pcd.paint_uniform_color(colors[index])
-    # o3d.visualization.draw_geometries(mesh_pcds)
-
     return map_labels
+
+
+def annotate_frames(data_path: str, output_path: str, map_pcd: o3d.geometry.PointCloud, map_labels: np.array):
+    map_kd_tree = KDTree(np.asarray(map_pcd.points))
+    with open(data_path) as data_file:
+        for index, line in enumerate(data_file):
+            frame_pcd = pcd_from_carla_line(line)
+            frame_labels = annotate_frame_with_map(frame_pcd, map_kd_tree, map_labels)
+            o3d.io.write_point_cloud(os.path.join(output_path, "{:06d}.pcd".format(index)), frame_pcd)
+            np.save(os.path.join(output_path, "{:06d}.npy".format(index)), frame_labels)
+
+
+def annotate_frame_with_map(frame_pcd: o3d.geometry.PointCloud, map_kd_tree: KDTree, map_labels: np.array) -> np.array:
+    frame_indices_in_map = map_kd_tree.query(np.asarray(frame_pcd.points), distance_upper_bound=0.5)[1]
+    # points with no reference will be marked with len(mapped_frame_pcd) index, so add zero to this index
+    map_labels = np.concatenate([map_labels, np.asarray([0])])
+    return map_labels[frame_indices_in_map]
 
 
 def process(data_path, annot_path, output_path):
@@ -107,7 +111,7 @@ def process(data_path, annot_path, output_path):
         map_labels = annotate_map(map_pcd, annot_path)
         np.save(annot_filename, map_labels)
 
-    visualize_pcd_labels(map_pcd, map_labels)
+    annotate_frames(data_path, output_path, map_pcd, map_labels)
 
 
 if __name__ == "__main__":
